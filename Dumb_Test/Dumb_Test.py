d994d13a71445842014db00cc40aa3ae91b51d92
Jon <jon.hu95@gmail.com>
2021-02-06 01:19:55 -0800

    Added in some awesome gifs

--- Dumb_Test/Dumb_Test.py
+++ Dumb_Test/Dumb_Test.py
@@ -11,16 +11,17 @@ class run_Game():
         self.create_Main_Screen()
         self.play_Music()
 
-        delay_count = 0 
+
+        self.delay_count = 0 
+        self.GIF_count = 0
         while True:     
              
-            self.change_BG(delay_count)
+            self.change_BG()
             for event in p.event.get():
                 if event.type == p.QUIT:
                     sys.exit()
-            delay_count+=1
-            if delay_count == 1000:
-                delay_count = 0
+            if self.delay_count == 1000:
+                self.delay_count = 0
 
             p.display.update() 
             p.display.flip()
@@ -28,34 +29,80 @@ class run_Game():
     def get_Paths(self):
         self.game_Path = os.path.dirname(os.path.realpath(__file__))
         self.music_Path = os.path.join(self.game_Path, 'Support', 'Music')
-
         self.pic_Path = os.path.join(self.game_Path, 'Support', 'Pictures')
+        self.GIF_Path = os.path.join(self.game_Path, 'Support', 'GIF')
     
     def create_Main_Screen(self):
-        self.screen = p.display.set_mode((500,500))
+        self.screen_X = 800
+        self.screen_Y = 500
+        self.screen = p.display.set_mode((self.screen_X, self.screen_Y))
         p.display.set_caption('Dumb Test')
 
     def play_Music(self):
         p.mixer.music.load(os.path.join(self.music_Path, 'opening_Theme.mp3'))
         p.mixer.music.play(-1) 
 
-    def change_BG(self, delay_count):
-        if delay_count%10 == 0:
-            color_1 = r.randint(0,255)
-            color_2 = r.randint(0,255)
-            color_3 = r.randint(0,255)
+    def change_BG(self):
+        #Keep changing the background to give the user epilepsy
+        delay_Multiplier = 100
+        if self.delay_count%delay_Multiplier == 0:
+            #Get center of screen
+            screen_Rect = self.screen.get_rect()
+            screen_Center = screen_Rect.center
+            [center_X, center_Y] = screen_Center
 
-            self.screen.fill((color_1, color_2, color_3))
 
-            title_image = p.image.load(os.path.join(self.pic_Path, 'title.png'))
-            main_title_mage = p.image.load(os.path.join(self.pic_Path, 'title_Main.png'))
-            title_x = r.randint(-500,500)
-            title_y = r.randint(-500,500)
-        
-            self.screen.blit(title_image, title_image.get_rect(center = self.screen.get_rect().center))
-            self.screen.blit(main_title_mage, (title_x,title_y))
-
+            #Load title images
+            title_Image = p.image.load(os.path.join(self.pic_Path, 'title_Support.png'))      
             
-        return False
+            glasses_GIF_Path = os.path.join(self.GIF_Path, 'Egg')
+            GIF_Files = os.listdir(glasses_GIF_Path)
+            if self.GIF_count == len(GIF_Files):
+                self.GIF_count = 0
+
+            title_BG = p.image.load(os.path.join(self.pic_Path, os.path.join(glasses_GIF_Path, GIF_Files[self.GIF_count])))
+            self.GIF_count+=1
+                
+            #Set Background
+            title_BG = p.transform.scale(title_BG, (self.screen_X, self.screen_Y))
+            self.screen.blit(title_BG, (0, 0))
+
+            #Change title color and coordinates
+            color_Title = title_Image.convert_alpha()            
+            
+            [min_X, max_X, min_Y, max_Y] = [-center_X,center_X*2,-center_Y,center_Y*2]
+            [title_X, title_Y] = self.random_Coordinates(min_X, max_X, min_Y, max_Y)
+            [title_X2, title_Y2] = self.random_Coordinates(min_X, max_X, min_Y, max_Y)
+            [title_X3, title_Y3] = self.random_Coordinates(min_X, max_X, min_Y, max_Y)
+
+            color_Title = self.change_Color(color_Title, self.random_Color())
+            self.screen.blit(color_Title, (title_X, title_Y))
+            color_Title = self.change_Color(color_Title, self.random_Color())
+            self.screen.blit(color_Title, (title_X2, title_Y2))
+            color_Title = self.change_Color(color_Title, self.random_Color())
+            self.screen.blit(color_Title, (title_X3, title_Y3))
+
+        self.delay_count += 1   
+
+    
+    def random_Color(self):
+        color_1 = r.randint(0,255)
+        color_2 = r.randint(0,255)
+        color_3 = r.randint(0,255)
+        return (color_1, color_2, color_3)
+
+    def change_Color(self, image, color):
+        coloured_Image = p.Surface(image.get_size())
+        coloured_Image.fill(color)
+    
+        finalImage = image.copy()
+        finalImage.blit(coloured_Image, (0, 0), special_flags = p.BLEND_MULT)
+        return finalImage
+    
+    def random_Coordinates(self, min_X, max_X, min_Y, max_Y):
+        x = r.randint(min_X, max_X)
+        y = r.randint(min_Y, max_Y)
+        return [x,y]
+
 if __name__ == '__main__':
     run_Game()
