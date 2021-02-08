c939d07a7293c78a3923c085706406e05c70a604
Jon <jon.hu95@gmail.com>
2021-02-07 18:48:39 -0800

    Deleting file that was added into config.py

--- Dumb_Test/Support/lib/get_Paths.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class get_Paths():
-    def __init__(self, file):
-        self.game_Path = os.path.dirname(os.path.realpath(file))
-        self.music_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
-        self.pic_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
-        self.GIF_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
