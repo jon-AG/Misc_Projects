07d82aca6aa9740c54e0999cac1f2fb11d618ab5
Jon <jon.hu95@gmail.com>
2021-02-07 16:56:53 -0800

    Moving media files and adding in get_Paths class

---
+++ Dumb_Test/Support/lib/get_Paths.py
@@ -0,0 +1,8 @@
+import os
+
+class get_Paths():
+    def __init__(self, file):
+        self.game_Path = os.path.dirname(os.path.realpath(file))
+        self.music_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
+        self.pic_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
+        self.GIF_Path = os.path.join(self.game_Path, 'Support', 'Media', 'Music')
